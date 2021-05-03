054579e970a521edc91035aa90cfcbbbfd9d8b4c
GabrieleManduchi <gabriele.manduchi@igi.cnr.it>
2021-05-03 21:36:20 +0000

    python compatibility

--- pydevices/RfxDevices/ARDUINO_MUX.py
+++ pydevices/RfxDevices/ARDUINO_MUX.py
@@ -35,7 +35,7 @@ class ARDUINO_MUX(Device):
               'options': ('no_write_shot',)}]
 
     def doSerialCommand(self, ser, cmd):
-        ser.write(bytes(cmd + '\n', 'utf-8'))
+        ser.write(bytes((cmd + '\n').encode('utf-8')))
         print(ser.readline())
         res = ser.readline()
         print(res)
@@ -43,7 +43,7 @@ class ARDUINO_MUX(Device):
         return res
  
     def doTcpCommand(self, sock, cmd):
-        sock.sendall(bytes(cmd + '\n', 'utf-8'))
+        sock.sendall(bytes((cmd + '\n').encode('utf-8')))
         resStr = ''
         while True:
             data = sock.recv(1)
@@ -76,10 +76,9 @@ class ARDUINO_MUX(Device):
                 print('Serial port not defined')
                 raise mdsExceptions.TclFAILED_ESSENTIAL
             try:
-                #ser = serial.Serial(port=serName, baudrate = 9600, timeout = .1)
-                #time.sleep(.5)
-                #ser.write(bytes('\n', 'utf-8'))
-                ser = 0
+                ser = serial.Serial(port=serName, baudrate = 9600, timeout = .1)
+                time.sleep(.5)
+                ser.write(bytes('\n'.encode('utf-8')))
             except:
                 print('Cannot open serial port '+serName)
                 raise mdsExceptions.TclFAILED_ESSENTIAL
@@ -90,12 +89,11 @@ class ARDUINO_MUX(Device):
                 print('IP address not defined')
                 raise mdsExceptions.TclFAILED_ESSENTIAL
             try:
-#                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#                sock.setsockopt( socket.IPPROTO_TCP, socket.TCP_NODELAY, 1 )
-#                server_address = (ipAddress, 23)
-#                print('connecting ...')
-#                sock.connect(server_address)
-                sock = 0
+                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                 sock.setsockopt( socket.IPPROTO_TCP, socket.TCP_NODELAY, 1 )
+                 server_address = (ipAddress, 23)
+                 print('connecting ...')
+                 sock.connect(server_address)
             except:
                 print('Cannot connect to '+ ipAddress)
                 raise mdsExceptions.TclFAILED_ESSENTIAL
@@ -118,23 +116,26 @@ class ARDUINO_MUX(Device):
                 bitsStr = format(bits, '04X')
                 currCommand = 'WM'+str(mpx+1)+str(m)+maskStr+bitsStr+'CM'+str(mpx+1)+format(m, '02X')
                 commands.append(currCommand)
-        if getattr(self, 'mux%d_read_back' % (mpx + 1)).data() == 'YES':
-            line1 = getattr(self, 'mux%d_rline1' % (mpx + 1)).data()
-            line2 = getattr(self, 'mux%d_rline2' % (mpx + 1)).data()
-            line3 = getattr(self, 'mux%d_rline3' % (mpx + 1)).data()
-            mask = (1 << line1)|(1 << line2)|(1 << line3)
-            maskStr = format(mask, '04X')
-            for m in range(8):
-                mm = m
-                bit1 = mm % 2
-                mm /= 2
-                bit2 = mm % 2
-                mm /= 2
-                bit3 = mm % 2
-                bits = (bit1 << line1)|(bit2 << line2) | (bit3 << line3)
-                bitsStr = format(bits, '04X')
-                currCommand = 'RRM'+str(mpx+1)+str(m)+maskStr+bitsStr+format(m, '02X')
-                commands.append(currCommand)
+            print( getattr(self, 'mux%d_read_back' % (mpx + 1)))
+            print( getattr(self, 'mux%d_read_back' % (mpx + 1)).data())
+            if getattr(self, 'mux%d_read_back' % (mpx + 1)).data() == 'YES':
+                print('CAZZONE')
+                line1 = getattr(self, 'mux%d_rline1' % (mpx + 1)).data()
+                line2 = getattr(self, 'mux%d_rline2' % (mpx + 1)).data()
+                line3 = getattr(self, 'mux%d_rline3' % (mpx + 1)).data()
+                mask = (1 << line1)|(1 << line2)|(1 << line3)
+                maskStr = format(mask, '04X')
+                for m in range(8):
+                    mm = m
+                    bit1 = mm % 2
+                    mm /= 2
+                    bit2 = mm % 2
+                    mm /= 2
+                    bit3 = mm % 2
+                    bits = (bit1 << line1)|(bit2 << line2) | (bit3 << line3)
+                    bitsStr = format(bits, '04X')
+                    currCommand = 'RRM'+str(mpx+1)+maskStr+bitsStr+format(m, '02X')
+                    commands.append(currCommand)
         commands.append('PRGSTOP')
         for command in commands:
             if isSerial:
@@ -149,7 +150,7 @@ class ARDUINO_MUX(Device):
         else:
             ans = self.doTcpCommand(ser, command)
         if self.mux1_read_back.data() == 'YES':
-            command = 'M1R'
+            command = 'RM1'
             if isSerial:
                 ans = self.doSerialCommand(ser, command)
             else:
@@ -164,8 +165,8 @@ class ARDUINO_MUX(Device):
             ans = self.doSerialCommand(ser, command)
         else:
             ans = self.doTcpCommand(ser, command)
-        if self.mux2_read_back.data() + 'YES':
-            command = 'M2R'
+        if self.mux2_read_back.data() == 'YES':
+            command = 'RM2'
             if isSerial:
                 ans = self.doSerialCommand(ser, command)
             else:
